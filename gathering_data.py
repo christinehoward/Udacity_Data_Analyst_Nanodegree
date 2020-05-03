a080bc1b15bb77ae9eda69fd91e77177cf7cf2a0
Christine <christinehoward@users.noreply.github.com>
2020-05-03 20:20:05 +0200

    Update gathering_data.py

--- gathering_data.py
+++ gathering_data.py
@@ -294,3 +294,188 @@ page = wptools.page('E.T._the_Extra-Terrestrial').get()
 # Accessing the image attribute will return the images for this page
 page.data['image']
 
+
+# JSON file structure
+# Javascript Object Notation
+# XML extensible mark up language
+
+# referencing JSON files in python is just like acessing dictionaries
+# JSON objects interpreted as dictionaries, arrays as lists
+
+infobox_json
+
+infobox_json['Box Office']
+# result: total box office
+infobox_json['Produced By']
+# result: 2 director names
+infobox_json['Release'][1]['Location']
+# there are 2 release dates, this accesses the 2nd release, and its location
+
+
+import wptools
+page = wptools.page('E.T._the_Extra-Terrestrial').get()
+
+# Quiz 1
+# Access the first image in the images attribute, which is a JSON array.
+
+page.data['image'][0]
+
+# Quiz 2
+# Access the director key of the infobox attribute, which is a JSON object.
+
+page.data['infobox']['director']
+
+
+# Gathering mashup solution
+
+import pandas as pd 
+import wptools 
+import os 
+import requests 
+from PIL import Image 
+from io import BytesIO
+
+title_list = [
+ 'The_Wizard_of_Oz_(1939_film)',
+ 'Citizen_Kane',
+ 'The_Third_Man',
+ 'Get_Out_(film)',
+ 'Mad_Max:_Fury_Road',
+ 'The_Cabinet_of_Dr._Caligari',
+ 'All_About_Eve',
+ 'Inside_Out_(2015_film)',
+ 'The_Godfather',
+ 'Metropolis_(1927_film)',
+ 'E.T._the_Extra-Terrestrial',
+ 'Modern_Times_(film)',
+ 'It_Happened_One_Night',
+ "Singin'_in_the_Rain",
+ 'Boyhood_(film)',
+ 'Casablanca_(film)',
+ 'Moonlight_(2016_film)',
+ 'Psycho_(1960_film)',
+ 'Laura_(1944_film)',
+ 'Nosferatu',
+ 'Snow_White_and_the_Seven_Dwarfs_(1937_film)',
+ "A_Hard_Day%27s_Night_(film)",
+ 'La_Grande_Illusion',
+ 'North_by_Northwest',
+ 'The_Battle_of_Algiers',
+ 'Dunkirk_(2017_film)',
+ 'The_Maltese_Falcon_(1941_film)',
+ 'Repulsion_(film)',
+ '12_Years_a_Slave_(film)',
+ 'Gravity_(2013_film)',
+ 'Sunset_Boulevard_(film)',
+ 'King_Kong_(1933_film)',
+ 'Spotlight_(film)',
+ 'The_Adventures_of_Robin_Hood',
+ 'Rashomon',
+ 'Rear_Window',
+ 'Selma_(film)',
+ 'Taxi_Driver',
+ 'Toy_Story_3',
+ 'Argo_(2012_film)',
+ 'Toy_Story_2',
+ 'The_Big_Sick',
+ 'Bride_of_Frankenstein',
+ 'Zootopia',
+ 'M_(1931_film)',
+ 'Wonder_Woman_(2017_film)',
+ 'The_Philadelphia_Story_(film)',
+ 'Alien_(film)',
+ 'Bicycle_Thieves',
+ 'Seven_Samurai',
+ 'The_Treasure_of_the_Sierra_Madre_(film)',
+ 'Up_(2009_film)',
+ '12_Angry_Men_(1957_film)',
+ 'The_400_Blows',
+ 'Logan_(film)',
+ 'All_Quiet_on_the_Western_Front_(1930_film)',
+ 'Army_of_Shadows',
+ 'Arrival_(film)',
+ 'Baby_Driver',
+ 'A_Streetcar_Named_Desire_(1951_film)',
+ 'The_Night_of_the_Hunter_(film)',
+ 'Star_Wars:_The_Force_Awakens',
+ 'Manchester_by_the_Sea_(film)',
+ 'Dr._Strangelove',
+ 'Frankenstein_(1931_film)',
+ 'Vertigo_(film)',
+ 'The_Dark_Knight_(film)',
+ 'Touch_of_Evil',
+ 'The_Babadook',
+ 'The_Conformist_(film)',
+ 'Rebecca_(1940_film)',
+ "Rosemary%27s_Baby_(film)",
+ 'Finding_Nemo',
+ 'Brooklyn_(film)',
+ 'The_Wrestler_(2008_film)',
+ 'The_39_Steps_(1935_film)',
+ 'L.A._Confidential_(film)',
+ 'Gone_with_the_Wind_(film)',
+ 'The_Good,_the_Bad_and_the_Ugly',
+ 'Skyfall',
+ 'Rome,_Open_City',
+ 'Tokyo_Story',
+ 'Hell_or_High_Water_(film)',
+ 'Pinocchio_(1940_film)',
+ 'The_Jungle_Book_(2016_film)',
+ 'La_La_Land_(film)',
+ 'Star_Trek_(film)',
+ 'High_Noon',
+ 'Apocalypse_Now',
+ 'On_the_Waterfront',
+ 'The_Wages_of_Fear',
+ 'The_Last_Picture_Show',
+ 'Harry_Potter_and_the_Deathly_Hallows_–_Part_2',
+ 'The_Grapes_of_Wrath_(film)',
+ 'Roman_Holiday',
+ 'Man_on_Wire',
+ 'Jaws_(film)',
+ 'Toy_Story',
+ 'The_Godfather_Part_II',
+ 'Battleship_Potemkin'
+]
+folder_name = 'bestofrt_posters'
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+# List of dictionaries to build and convert to a DataFrame later
+df_list = []
+image_errors = {} # 
+for title in title_list:
+    try: # try and except go together, code will not break between try and except, instead errors will go to the except portion
+        # This cell is slow so print ranking to gauge time remaining
+        ranking = title_list.index(title) + 1 # includes movie's ranking within top 100, need to add 1 because of 0 indexing
+        print(ranking)
+        page = wptools.page(title, silent=True) # silent=True means do not echo page data if true, saying not to print while running
+        images = page.get().data['image']
+        # First image is usually the poster
+        first_image_url = images[0]['url']
+        r = requests.get(first_image_url)
+        # download movie poster image
+        i = Image.open(BytesIO(r.content)) # what does the BytesIO do again?
+        image_file_format = first_image_url.split('.')[-1] # split at the ., cutting off the very end .jpeg (for example), wanted to clean links
+        i.save(folder_name) + "/" + str(ranking) + "_" + title + '.' + image_file_format)
+        # Append to list of dictionaries
+        df_list.append({'ranking': int(ranking)
+                        'title': title,
+                        'poster_url': first_image_url})
+# Not best practice to catch all exceptions but fine for this short script
+    except Exception as e:
+        print(str(ranking) + '_' + title + ": " + str(e))
+        image_errors[str(ranking) + "_" + title] = images
+
+# One you have completed the above code requirements, read and run the three cells below and interpret their output.
+for key in image_errors.keys():
+    print(key)
+
+# Inspect unidentifiable images and download them individually
+for rank_title, images in image_errors.items():
+    if rank_title == '22_A_Hard_Day%27s_Night_(film)':
+        url = 'https://upload.wikimedia.org/wikipedia/en/4/47/A_Hard_Days_night_movieposter.jpg'
+    title = rank_title[3:]
+    df_list.append({'ranking': int(title_list.index(title) + 1),
+                    'title' : title,
+                    'poster_url': url})
